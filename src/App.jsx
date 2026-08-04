496dc9dc1a71c279f035edeec0c0116abcaea1d4
Nate K <nathaniel.karau@gmail.com>
2026-08-04 15:36:48 +0000

    Created HomeHub pantry starter page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>HomeHub Pantry Organizer</h1>
+
+      <h2>Inventory</h2>
+
+      <p>Milk - 1 gallon</p>
+      <p>Rice - 10 lbs</p>
+      <p>Eggs - 12</p>
+
+    </div>
+  )
+}
+
+export default App
